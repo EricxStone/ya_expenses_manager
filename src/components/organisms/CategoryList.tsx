27d6dd6c1031fa212f6b8f4c6360cfca9cb439ae
EricxStone <ericshek1022@gmail.com>
2021-09-20 02:04:10 +0800

    Update spending calculation logic

--- src/components/organisms/CategoryList.tsx
+++ src/components/organisms/CategoryList.tsx
@@ -1,8 +1,11 @@
 import React, {FunctionComponent} from 'react'
 import {List, Center, VStack, HStack, Text, Button, Heading, useToken, Box} from 'native-base'
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import { useSelector } from 'react-redux';
+import {RootState} from '../../store/store';
 import {CategoryLine} from '_molecules'
-import {Category} from '_models'
+import {Category, Transaction} from '_models'
+import { DateTime } from 'luxon';
 
 export interface Props{
     categories: Category[];
@@ -11,8 +14,25 @@ export interface Props{
     onCategoryEdit: (category: Category) => void;
 }
 
+const startOfMonth: number = DateTime.local().startOf("month").toMillis();
+const endOfMonth: number = DateTime.local().endOf("month").toMillis();
+
 const CategoryList: FunctionComponent<Props> = ({categories, listHeader, onCategoryClick, onCategoryEdit}: Props) => {
+    const transactionList: Transaction[] = useSelector((state: RootState) => state.transactions.transactionList);
+
+    const calculateSpending = (categoryId: string) => {
+        let spending: number = 0;
+        transactionList.map((item: Transaction) => {
+            if (item.categoryId == categoryId && item.transactionDate > startOfMonth && item.transactionDate < endOfMonth)
+                spending += item.amount;
+        })
+        return spending;
+    }
+
+
     const categoryList: JSX.Element[] | JSX.Element = categories.length > 0 ? categories.map((item: Category) => {
+        item.spending = calculateSpending(item.id);
+        if (item.budget > 0) item.remaining = item.budget - item.spending;
         return <CategoryLine key={item.id} category={item} onCategoryClick={onCategoryClick} onCategoryEdit={onCategoryEdit}></CategoryLine>
     }) : <Center flex={1}><Text fontSize="lg" bold={true}>-</Text></Center>
     const [darkText] = useToken('colors', [
