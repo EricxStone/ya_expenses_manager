d4031b6bc82a996f1d461b4e2087d6ad807a61e3
EricxStone <ericshek1022@gmail.com>
2021-09-23 08:25:23 +0800

    Add remove transaction function

--- src/scenes/inputTransaction/index.tsx
+++ src/scenes/inputTransaction/index.tsx
@@ -13,11 +13,15 @@
      Center,
      Text,
      ScrollView,
+     Button,
+     Icon,
  } from 'native-base'
  import { useDispatch, useSelector } from 'react-redux';
  import { RouteProp, NavigationProp } from '@react-navigation/native';
- import {StackNavigationProp} from '@react-navigation/stack'
- import {RootStackParamList} from '../../index'
+ import {StackNavigationProp} from '@react-navigation/stack';
+ import {RootStackParamList} from '../../index';
+ import FontAwesome from 'react-native-vector-icons/FontAwesome5';
+ import {Alert} from "react-native";
 
  type InputTransactionScreenRouteProp = RouteProp<
     RootStackParamList,
@@ -38,9 +42,11 @@ export interface Props{
 
     const newTransaction = route.params.transaction === undefined ? new Transaction("", 0, "", 0, "") : route.params.transaction;
     const headingName = route.params.transaction === undefined ? "Add a Transaction" : "Edit Transaction";
-    const dispatch = useDispatch()
-    // const originalCategoryId: string = newTransaction.categoryId;
-    // const originalAmount: number = newTransaction.amount;
+    const dispatch = useDispatch();
+
+    let deleteTransactionButton: JSX.Element = (
+        <></>
+    )
 
     const onInputSubmit = (transaction: Transaction) => {
         console.log("transaction submit:", transaction);
@@ -53,6 +59,33 @@ export interface Props{
         }
         navigation.goBack();
     }
+
+    const onInputDelete = () => {
+        if (route.params.transaction !== undefined) {
+            const transaction = route.params.transaction;
+            Alert.alert(
+                "Delete Transaction?",
+                "",
+                [
+                  {
+                    text: "Cancel",
+                    style: "cancel"
+                  },
+                  { text: "Delete", onPress: () => {
+                    dispatch({type: 'DELETE_TRANSACTION', payload: transaction.id});
+                    navigation.goBack();
+                  } }
+                ]
+            );
+        }
+    }
+
+    if (route.params.transaction !== undefined) 
+        deleteTransactionButton = (
+            <Button bgColor="white" onPress={onInputDelete}>
+                <Icon color="darkText" as={<FontAwesome name="trash-alt" />} size="sm" />
+            </Button>
+        );
 
     React.useEffect(() => {
         return () => {
@@ -67,7 +100,8 @@ export interface Props{
             <Box>
                 <HStack bg='white' px={1} py={3} justifyContent='space-between' alignItems='center'>
                     <HStack space={4} px={3} alignItems='center'>
-                        <Text color="blue.800" fontSize="xl" fontWeight='bold'>{headingName}</Text>
+                        <Text color="blue.800" fontSize="xl" fontWeight='bold' w="85%">{headingName}</Text>
+                        {deleteTransactionButton}
                     </HStack>
                 </HStack>
             </Box>
